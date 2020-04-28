49b105201638a3847cc74ea7d1caac517370ac63
Mahendra <m.kumar@thesynapses.com>
2020-04-28 16:42:24 +0530

    google keys

--- index.js
+++ index.js
@@ -2,13 +2,26 @@ const express = require('express');
 const app = express();
 const { google } = require('googleapis');
 const {SecretManagerServiceClient} = require('@google-cloud/secret-manager');
-//const {GoogleAuth} = require('google-auth-library')
-const client = new SecretManagerServiceClient ()
+// const {GoogleAuth} = require('google-auth-library')
+const client = new SecretManagerServiceClient();
 
-client.accessSecretVersion('projects/626923387764/secrets/testcred')
-.then(data=>{
-    console.log(data)
-})
+// async function getProjectId(){
+//   const auth = new GoogleAuth({
+//     scopes: 'https://www.googleapis.com/auth/cloud-platform',
+//   })
+
+//   const projectId = await auth.getProjectId().catch(()=>{
+//     return projectId
+//   })
+  
+// }
+
+
+// client.accessSecretVersion('projects/626923387764/secrets/testcred/versions/1')
+// .then(data=>{
+//     console.log(data)
+// })
+
 // exports.secrets ={}
 
 // exports.load = async () => {
@@ -64,6 +77,54 @@ client.accessSecretVersion('projects/626923387764/secrets/testcred')
 
 
 app.get('/', (req, res) => {
+
+
+/**
+ * TODO(developer): Uncomment these variables before running the sample.
+ */
+// parent = 'projects/my-project', // Project for which to manage secrets.
+// secretId = 'foo', // Secret ID.
+// payload = 'hello world!' // String source data.
+
+async function createAndAccessSecret() {
+  // Create the secret with automation replication.
+  // const [secret] = await client.createSecret({
+  //   parent: parent,
+  //   secret: {
+  //     name: secretId,
+  //     replication: {
+  //       automatic: {},
+  //     },
+  //   },
+  //   secretId,
+  // });
+
+  // console.info(`Created secret ${secret.name}`);
+
+  // Add a version with a payload onto the secret.
+  // const [version] = await client.addSecretVersion({
+  //   parent: secret.name,
+  //   payload: {
+  //     data: Buffer.from(payload, 'utf8'),
+  //   },
+  // });
+
+  // console.info(`Added secret version ${version.name}`);
+
+  // Access the secret.
+  const [accessResponse] = await client.accessSecretVersion({
+    name: 'projects/626923387764/secrets/testcred/versions/1',
+  });
+
+  const responsePayload = accessResponse.payload.data.toString('utf8');
+  console.info(`Payload: ${responsePayload}`);
+}
+createAndAccessSecret();
+
+
+
+
+
   console.log('Hello world newreceived a request.');
   
   const target = process.env.TARGET || 'World test1';
