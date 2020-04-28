a898976f582229efe5dc42bc395c692f08c9cd34
Mahendra <m.kumar@thesynapses.com>
2020-04-28 15:05:52 +0530

    added secret

--- index.js
+++ index.js
@@ -2,49 +2,70 @@ const express = require('express');
 const app = express();
 const { google } = require('googleapis');
 const {SecretManagerServiceClient} = require('@google-cloud/secret-manager');
-const client = new SecretManagerServiceClient();
-const name = getSecretName('projects/626923387764/secrets/testcred');
+//const {GoogleAuth} = require('google-auth-library')
+const client = new SecretManagerServiceClient ()
 
-const auth = new google.auth.GoogleAuth({});
+client.accessSecretVersion('projects/626923387764/secrets/testcred/versions/latest')
+.then(data=>{
+    console.log(data)
+})
+// exports.secrets ={}
+
+// exports.load = async () => {
+//   const [data] = await client.accessSecretVersion({ name: 'projects/626923387764/secrets/testcred/versions/latest'})
+//   const secret = data.payload.data.toString()
+//   console.log(secret)
+//   module.exports.secrets ['testcred']
+// }
 
 
-const SECRET_KEYS = {
-  ENV: `secrets/test-env/versions/latest`
-}
+
+
+
+
+// const client = new SecretManagerServiceClient();
+// const name = getSecretName('projects/626923387764/secrets/testcred');
+
+// const auth = new google.auth.GoogleAuth({});
+
+
+// const SECRET_KEYS = {
+//   ENV: `secrets/test-env/versions/latest`
+// }
 // projects/240998304781/secrets/sa-key-dev/versions/1
 // Promise to get current projectID from with the server
-const getProjectIdPromise = new Promise(async function (resolve) {
-  try {
-    const projectId = await auth.getProjectId();
-    return resolve(projectId);
-  } catch (e) {
-    console.log(e);
-    process.exit(1);
-  }
-});
+// const getProjectIdPromise = new Promise(async function (resolve) {
+//   try {
+//     const projectId = await auth.getProjectId();
+//     return resolve(projectId);
+//   } catch (e) {
+//     console.log(e);
+//     process.exit(1);
+//   }
+// });
 
 // Async method to get the full key identifier
-async function getSecretName(keySuffix) {
-  const projectId = await getProjectIdPromise();
+// async function getSecretName(keySuffix) {
+//   const projectId = await getProjectIdPromise();
 
-  return `projects/${projectId}/${keySuffix}`
-}
+//   return `projects/${projectId}/${keySuffix}`
+// }
 
 // Method to extract the secret
 // You can run this in build, deployment or run time! (Anytime!)
-exports.getEnvAsync = async function() {
-  const name = await getSecretName(SECRET_KEYS.ENV);
-  const [secret] = await client.accessSecretVersion({ name });
-   console.log("requestforname",name);
-  const payload = secret.payload.data.toString('utf8');
-	console.log(payload);
-  return payload
-}
+// exports.getEnvAsync = async function() {
+//   const name = await getSecretName(SECRET_KEYS.ENV);
+//   const [secret] = await client.accessSecretVersion({ name });
+//    console.log("requestforname",name);
+//   const payload = secret.payload.data.toString('utf8');
+// 	console.log(payload);
+//   return payload
+// }
 
 
 app.get('/', (req, res) => {
   console.log('Hello world newreceived a request.');
-  var test = getEnvAsync();
+  
   console.log(test);
   const target = process.env.TARGET || 'World test1';
    console.log()
